6f43993d03fd624138efebe8ecd9ed1272108296
gadd0609 <alegaddiel@gmail.com>
2024-05-30 01:43:13 -0600

    Fix(welcome): Header fix

--- src/components/features/index.vue
+++ src/components/features/index.vue
@@ -1,37 +1,35 @@
 <template>
-  <div class="flex transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
-    <div class="x-auto grid-cols-1 items-center px-4 sm:px-6 sm:py-10 lg:max-w-5xl lg:grid-cols-1">
-      <div class="-mt-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
-      <div class="w-max rounded-2xl bg-gray-50 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+  <div>
+    <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl md:py-5">TOKENOMICS</h2>
+  </div>
+  <div class="flex flex-col justify-center gap-6 sm:flex-row transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-center items-center mx-auto max-w-7xl px-6 lg:px-8 sm:px-6 sm:py-10 lg:max-w-5xl">
+      <!-- Card 1 -->
+      <div class="lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
         <div class="mx-auto max-w-xs px-8">
           <img src="/public/Wild3.png" alt="">
           <p class="mt-6 flex items-baseline justify-center gap-x-2">
             <span class="text-3xl font-bold tracking-tight text-gray-900">Objetive</span>
           </p>
           <p class="mt-6 text-xs leading-5 text-gray-600">
-            As an initial project WildCatCoin plans to launch a Play2Earn game with the nostalgia factor based on retro web games to make it fun and nostalgic when playing.
-            WildCatCoin is committed that our contract will be designed to be safe and secure within our royal alliance.
-            The goal of WildCatCoin is to create an alliance with the community of trust and prosperity.
+            As an initial project WildCatCoin plans to launch a Play2Earn game with the nostalgia factor based on retro web games to make it fun and nostalgic when playing. WildCatCoin is committed that our contract will be designed to be safe and secure within our royal alliance. The goal of WildCatCoin is to create an alliance with the community of trust and prosperity.
           </p>
         </div>
       </div>
-    </div>
-    <div class="-mt-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
-      <div class="w-80 rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+      <!-- Card 2 -->
+      <div class="lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
         <div class="mx-auto max-w-xs px-8">
-          <img src="/public/spaceship.png" alt="">
           <p class="mt-6 flex items-baseline justify-center gap-x-2">
             <span class="text-3xl font-bold tracking-tight text-gray-900">Staking and Reflection</span>
           </p>
           <p class="mt-6 text-xs leading-5 text-gray-600">
-            WildCatCoin holders will earn 20% APY for keeping their $WILDCATS tokens on the APP. 
-            the alliance offers an airdrop of 1% for each transaction made in the currency, that means that each holder will be able to have this percentage for the simple fact of holding the currency in their wallet
+            WildCatCoin holders will earn 20% APY for keeping their $WILDCATS tokens on the APP. the alliance offers an airdrop of 1% for each transaction made in the currency, that means that each holder will be able to have this percentage for the simple fact of holding the currency in their wallet
           </p>
+          <img src="/public/spaceship.png" alt="">
         </div>
       </div>
-    </div>
-    <div class="-mt-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
-      <div class="w-80 rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+      <!-- Card 3 -->
+      <div class="lg:w-full lg:max-w-md lg:flex-shrink-0 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100">
         <div class="mx-auto max-w-xs px-8">
           <img src="/public/wink cat.png" alt="">
           <p class="mt-6 flex items-baseline justify-center gap-x-2">
@@ -44,9 +42,7 @@
       </div>
     </div>
   </div>
-  </div>
 </template>
-
 <script>
 import setup from './hook.js'
 export default {
--- src/components/welcome/index.vue
+++ src/components/welcome/index.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="sticky top-0 z-50 bg-white transition">
-    <div class="container mx-auto flex justify-between items-center md:px-4 lg:px-16 py-4">
+  <div class="sticky top-0 z-50">
+    <div class="container mx-auto flex justify-center md:px-4 lg:px-16 py-4">
       <div class="flex items-center space-x-4 flex-shrink-0">
         <img src="/public/WildCatCoin.png" alt="Logo" class="h-12 md:h-16 lg:h-20">
         <span class="text-2xl md:text-3xl lg:text-4xl font-extrabold text-gray-900">WILDCATCOIN</span>
       </div>
-      <nav :class="menuOpen ? 'block' : 'hidden' " class="hidden lg:flex lg:space-x-4 items-center space-x-4 flex-wrap">
-        <a href="#" class="text-gray-600 hover:text-gray-800 transition duration-300">Features</a>
-        <a href="#" class="text-gray-600 hover:text-gray-800 transition duration-300">How to buy</a>
-        <a href="#" class="text-gray-600 hover:text-gray-800 transition duration-300">Roadmap</a>
-      </nav>
-      <div class="lg:hidden">
-        <button @click="toggleMenu" class="text-gray-800">
-          <svg class="h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M3 6h16M 12h16m-7 6h7"></path>
-          </svg>
-        </button>
-      </div>
   </div>
 </div>
-  <div class="animate-bounce">
+  <div>
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2" />
@@ -31,14 +19,12 @@
           <p class="mt-6 text-xl leading-8 text-gray-600">Welcome to</p>
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">WILDCATCOIN</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">Be part of the wild cats alliance.</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-          </div>
         </div>
       </div>
       <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+        <div 
+        class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#a5d6e2] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
     </div>
   </div>
@@ -46,6 +32,7 @@
 
 <script>
 import setup from './hook'
+
 export default {
     name: "welcomeComponent",
     setup
